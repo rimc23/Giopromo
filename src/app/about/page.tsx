abc4025c550f98b0c39eed17cb7ad64ea86263d6
Hector <hector.dev.ui@gmail.com>
2024-10-07 00:08:09 -0600

    imports

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/Header";
+import Header from '@/components/Header';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Footer from "@/components/Footer";
